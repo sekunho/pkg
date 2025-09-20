788aff1d7837eca3360bc91cd2d5a74e06acf9c7
SEKUN <software@sekun.net>
2025-09-20 12:58:24 +0800

    refactor(su_template): Remove thiserror

--- su_template/src/lib.rs
+++ su_template/src/lib.rs
@@ -6,7 +6,6 @@ use minijinja_autoreload::AutoReloader;
 use serde::Serialize;
 
 pub use minijinja::{context, Error};
-use thiserror::Error;
 
 pub struct HandleBuilder {
     env: Environment<'static>,
@@ -20,39 +19,183 @@ pub enum Handle {
     Autoreload(Arc<AutoReloader>),
 }
 
-#[derive(Debug, Error)]
-#[error("failed to fetch/render template {0}")]
-pub struct TemplateError(#[from] minijinja::Error);
+#[derive(Debug)]
+pub struct TemplateError(minijinja::Error);
 
-#[derive(Debug, Error)]
-#[error(transparent)]
-pub struct EnvError(#[from] minijinja::Error);
+impl std::error::Error for TemplateError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+}
 
-#[derive(Debug, Error)]
-#[error(transparent)]
-pub struct GetTemplateError(#[from] minijinja::Error);
+impl From<minijinja::Error> for TemplateError {
+    fn from(value: minijinja::Error) -> Self {
+        Self(value)
+    }
+}
 
-#[derive(Debug, Error)]
+impl std::fmt::Display for TemplateError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+#[derive(Debug)]
+pub struct EnvError(minijinja::Error);
+
+impl std::error::Error for EnvError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+
+    fn cause(&self) -> Option<&dyn std::error::Error> {
+        self.source()
+    }
+}
+
+impl From<minijinja::Error> for EnvError {
+    fn from(value: minijinja::Error) -> Self {
+        Self(value)
+    }
+}
+
+impl std::fmt::Display for EnvError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+#[derive(Debug)]
+pub struct GetTemplateError(minijinja::Error);
+
+impl std::error::Error for GetTemplateError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+
+    fn cause(&self) -> Option<&dyn std::error::Error> {
+        self.source()
+    }
+}
+
+impl From<minijinja::Error> for GetTemplateError {
+    fn from(value: minijinja::Error) -> Self {
+        Self(value)
+    }
+}
+
+impl std::fmt::Display for GetTemplateError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+#[derive(Debug)]
 pub enum RenderTemplateError {
-    #[error("failed to acquire environment. reason: {0}")]
-    EnvError(#[from] EnvError),
-    #[error("failed to get template from environment. reason: {0}")]
-    GetTemplateError(#[from] GetTemplateError),
-    #[error("failed to render template with the provided context. reason: {0}")]
-    RenderError(#[from] RenderError),
+    EnvError(EnvError),
+    GetTemplateError(GetTemplateError),
+    RenderError(RenderError),
 }
 
-#[derive(Debug, Error)]
+impl std::error::Error for RenderTemplateError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+
+    fn cause(&self) -> Option<&dyn std::error::Error> {
+        self.source()
+    }
+}
+
+impl std::fmt::Display for RenderTemplateError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RenderTemplateError::EnvError(env_error) => write!(f, "failed to acquire environment {}", env_error),
+            RenderTemplateError::GetTemplateError(get_template_error) => write!(f, "unable to get template from env {}", get_template_error),
+            RenderTemplateError::RenderError(render_error) => write!(f, "error in rendering with the provided context {}", render_error),
+        }
+    }
+}
+
+impl From<EnvError> for RenderTemplateError {
+    fn from(value: EnvError) -> Self {
+        Self::EnvError(value)
+    }
+}
+
+impl From<GetTemplateError> for RenderTemplateError {
+    fn from(value: GetTemplateError) -> Self {
+        Self::GetTemplateError(value)
+    }
+}
+
+impl From<RenderError> for RenderTemplateError {
+    fn from(value: RenderError) -> Self {
+        Self::RenderError(value)
+    }
+}
+
+#[derive(Debug)]
 pub enum RenderTemplateStrError {
-    #[error("failed to acquire environment. reason: {0}")]
-    EnvError(#[from] EnvError),
-    #[error("failed to render template string with the provided context. reason: {0}")]
-    RenderError(#[from] RenderError),
+    EnvError(EnvError),
+    RenderError(RenderError),
 }
 
-#[derive(Debug, Error)]
-#[error(transparent)]
-pub struct RenderError(#[from] minijinja::Error);
+impl std::error::Error for RenderTemplateStrError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+
+    fn cause(&self) -> Option<&dyn std::error::Error> {
+        self.source()
+    }
+}
+
+impl From<EnvError> for RenderTemplateStrError {
+    fn from(value: EnvError) -> Self {
+        Self::EnvError(value)
+    }
+}
+
+impl From<RenderError> for RenderTemplateStrError {
+    fn from(value: RenderError) -> Self {
+        Self::RenderError(value)
+    }
+}
+
+impl std::fmt::Display for RenderTemplateStrError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RenderTemplateStrError::EnvError(env_error) => write!(f, "failed to acquire environment {}", env_error),
+            RenderTemplateStrError::RenderError(render_error) => write!(f, "error in rendering with the provided context {}", render_error),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct RenderError(minijinja::Error);
+
+impl std::error::Error for RenderError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+
+    fn cause(&self) -> Option<&dyn std::error::Error> {
+        self.source()
+    }
+}
+
+impl From<minijinja::Error> for RenderError {
+    fn from(value: minijinja::Error) -> Self {
+        Self(value)
+    }
+}
+
+impl std::fmt::Display for RenderError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
 impl HandleBuilder {
     /// Returns a mutable borrow to the internal environment. This may be useful
